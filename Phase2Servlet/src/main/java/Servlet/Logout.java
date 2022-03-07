5eddb19d3fcc88fbb650dca63b570a5b487a0d33
Sudeep <Sudeep@DESKTOP-6OM508S>
2022-03-08 00:17:21 +0530

    phase2servlet

---
+++ Phase2Servlet/src/main/java/Servlet/Logout.java
@@ -0,0 +1,39 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+//		Invalidate the session here
+		request.getSession().invalidate();
+		
+		out.println("<h2>You are now logged out... Successfully<br>");
+		
+		out.println("<a href='index.html'>if you want to Login again</a>");
+	}
+
+}
+
+
+
+
+
